f4b3d846ec3b521b8cfa875abbd8e95c58165fa3
manoharreddyshanmukha <shanmukamanohar2018@gmail.com>
2026-02-25 00:29:03 +0530

    Use environment DATABASE_URL for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Manohar%402003@localhost:5432/cognitive_auth"
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_URL = os.getenv("postgresql://cognitive_auth_db_user:ypDWxA6hib9F9geQWd7OUIbMdgAYrMJa@dpg-d6ev7pp5pdvs73f39h5g-a/cognitive_auth_db")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
